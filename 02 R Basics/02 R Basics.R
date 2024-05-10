cf05b0f49eaeec2d9b1383b2fdfb04c664c332c7
Muhammad Shahzaib Yaqoob <158167657+mshahzaib4@users.noreply.github.com>
2024-05-10 10:25:05 +0500

    Add files via upload

---
+++ 02 R Basics/02 R Basics.R
@@ -0,0 +1,51 @@
+
+
+print("Muhammad Shahzaib Yaqoob")
+
+
+# This is a comment"#" in R and it ignored by the interpreter
+
+"
+Multi-line comment.
+Line 1
+Line 2
+Line 3
+"
+
+# Variables in R Language
+
+Varibale<-44
+B<-16
+
+# Data Types
+Varibale<-11.5
+class(Varibale) # Class is used to check datatype
+
+Varibale<-11L
+class(Varibale)
+
+Varibale_bool<-TRUE
+class(Varibale_bool)
+
+# Numbers and Strings in R Language
+
+x <- 10
+y <- 3.14
+
+# Mathematical operations
+x + y
+
+# String
+str1 <- 'Hello!, Muhammad'
+
+str2 <- "Shahzaib Yaqoob"
+paste(str1,str2)
+
+# Assignment Operators
+str1 <- 'Hello!, Muhammad'
+
+str2 -> "Shahzaib Yaqoob"
+str3 = "Shahzaib Yaqoob"
+
+
+
